81473bd37eed95edcb4b1a98bfa5ea7e78f9eaeb
Pedro Gazola <ph-gazola@hotmail.com>
2022-10-27 19:17:54 -0300

    Obtendo os 10 cursos mais curtidos

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -27,7 +27,19 @@ export const coursesController = {
                 return res.status(400).json( {message: error.message})
             }
         }
-    },  
+    },
+
+    //GET /courses/popular
+    popular: async (req: Request, res: Response) => {
+        try {
+            const topTen = await courseService.getTopTenbyLike()
+            return res.json(topTen)
+        } catch (error) {
+            if(error instanceof Error) {
+                return res.status(400).json( {message: error.message})
+            }
+        }
+    },
 
     //GET /courses/search?name=
     search: async(req: Request, res: Response) => {
--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,8 +1,9 @@
 import { Op } from "sequelize"
 import { Course } from "../models"
+import { likeService } from './likeService'
 
 export const courseService = {
-    findByIdWithEpisode: async(id: string) => {
+    findByIdWithEpisode: async (id: string) => {
         const courseWithEpisode = await Course.findByPk(id, {
             attributes: [
                 'id',
@@ -26,7 +27,7 @@ export const courseService = {
         })
         return courseWithEpisode
     },
-    getRandomFeaturedCourses: async() => {
+    getRandomFeaturedCourses: async () => {
         const featuredCourseds = await Course.findAll({
             attributes: [
                 'id',
@@ -34,20 +35,44 @@ export const courseService = {
                 'synopsis',
                 ['thumbnail_url', 'thumbnailUrl']
             ],
-            where:{
+            where: {
                 featured: true
             }
         })
-        const randomFeaturedCourses = featuredCourseds.sort(()=> 0.5 - Math.random())
+        const randomFeaturedCourses = featuredCourseds.sort(() => 0.5 - Math.random())
 
         return randomFeaturedCourses.slice(0, 3)
     },
-    getTopTenNewestCourses: async() => {
+    getTopTenNewestCourses: async () => {
         const courses = await Course.findAll({
             limit: 10,
             order: [['created_at', 'DESC']],
         })
         return courses
+    },
+    getTopTenbyLike: async () => {
+        const results = await Course.sequelize?.query(
+            `SELECT
+                courses.id,
+                courses.name,
+                courses.synopsis,
+                courses.thumbnail_url as thumbnailUrl,
+            COUNT(users.id) AS likes
+            FROM courses
+                LEFT OUTER JOIN likes
+                    ON courses.id = likes.course_id
+                    INNER JOIN users
+                        ON users.id = likes.user_id
+            GROUP BY courses.id
+            ORDER BY likes DESC
+            LIMIT 10;`  
+        )
+        if (results) {
+            const [topTen, metada] = results
+            return topTen
+        } else {
+            return null
+        }
     },
     findByName: async (name: string, page: number, perPage: number) => {
         const offset = (page - 1) * perPage
@@ -61,7 +86,7 @@ export const courseService = {
             where: {
                 name: {
                     [Op.iLike]: `%${name}%`
-                } 
+                }
             },
             limit: perPage,
             offset
